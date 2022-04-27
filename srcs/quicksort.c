06f02807ad516df72f3abe963bf9c5de6e629f9d
Robin KERJEAN <rokerjea@e3r13p2.clusters.42paris.fr>
2022-04-27 17:30:22 +0200

    progres mais bof

--- srcs/quicksort.c
+++ srcs/quicksort.c
@@ -14,20 +14,25 @@
 
 t_median	push_to_other_stack(t_data *stackgiver, t_data *stackreceiver, unsigned int chunksize)
 {
-	printf ("start of push med quart with size of %d\n", chunksize);
+	//printf ("start of push med quart with size of %d\n", chunksize);
 	int median;
 	int	lowquartian;
 	t_median	nextchunk;
+	int	i;
 
+	i = 0;
 	nextchunk.a = 0;
 	nextchunk.b = 0;
 	median = findmedianofchunk(stackgiver->first, chunksize);
 	lowquartian = findquartianofchunk(stackgiver->first, chunksize);
-	printf ("median = %d, quartian = %d\n", median, lowquartian);
-	while (smallerintinstack(median, stackgiver))
+	//printf ("median = %d, quartian = %d\n", median, lowquartian);
+	while (smallerintinstack(median, stackgiver) && chunksize)
 	{
 		if (stackgiver->first->num > median)
+		{
 			named_ope_rotate(stackgiver);//prob here!!
+			i++;
+		}
 		if (stackgiver->first->num <= median)
 		{
 			if (stackgiver->first->num <= lowquartian)//inferior quart to top of other stack
@@ -42,24 +47,25 @@ t_median	push_to_other_stack(t_data *stackgiver, t_data *stackreceiver, unsigned
 				named_ope_rotate(stackreceiver);
 			}
 		}
+		chunksize--;
 	}
 	while (stackreceiver->last->num > lowquartian)
 		named_ope_revrotate(stackreceiver);//get sup quart to top of stack, to keep sort
-	/*while (stackreceiver->last->num > median)
-		named_ope_revrotate(stackgiver);*/
-	printf ("end of push med quart, chunk a = %d, b = %d\n", nextchunk.a, nextchunk.b);
+	while (i--)
+		named_ope_revrotate(stackgiver);
+	//printf ("end of push med quart, chunk a = %d, b = %d\n", nextchunk.a, nextchunk.b);
 	return (nextchunk);
 }
 
 void	push_sorted_chunk(t_data *stackgiver, t_data *stackreceiver, unsigned int chunksize)
 {
-	printf ("start of push sorted size = %d\n", chunksize);
+	///printf ("start of push sorted size = %d\n", chunksize);
 	while (chunksize)
 	{
 		named_ope_push(stackgiver, stackreceiver);
 		chunksize--;
 	}
-	printf ("end of push sorted\n");
+	//printf ("end of push sorted\n");
 }
 
 /*
@@ -73,8 +79,8 @@ void	push_sorted_chunk(t_data *stackgiver, t_data *stackreceiver, unsigned int c
 */
 void	double_pi_quicksort(t_data *stackgiver, t_data *stackreceiver, unsigned int chunksize)
 {
-	printf ("start of double_pi_quicksort with size of %d\n", chunksize);
-	ft_printstacks(stackgiver, stackreceiver);
+	//printf ("start of double_pi_quicksort with size of %d\n", chunksize);
+	//ft_printstacks(stackgiver, stackreceiver);
 	t_median	nextchunk;
 
 	nextchunk.a = 0;
@@ -95,22 +101,22 @@ void	double_pi_quicksort(t_data *stackgiver, t_data *stackreceiver, unsigned int
 		nextchunk = push_to_other_stack(stackgiver, stackreceiver, chunksize);
 	if (isstacksort(stackgiver, chunksize) != 1)
 	{
-		printf ("start of new quicksort with size of %d\n", chunksize -(nextchunk.a + nextchunk.b));
+		//printf ("start of new quicksort with size of %d\n", chunksize -(nextchunk.a + nextchunk.b));
 		double_pi_quicksort(stackgiver, stackreceiver, chunksize -(nextchunk.a + nextchunk.b));//next quicksort for rest of stack
 	}
 	//get lowerchunks back in full
 	if (nextchunk.a > 1)
 	{
-		printf("gate1\n");
+		//printf("gate1\n");
 		double_pi_quicksort(stackreceiver, stackgiver, nextchunk.a);//next quicksort for superior quart		
 	}
 	push_sorted_chunk(stackreceiver, stackgiver, nextchunk.a);
 	if (nextchunk.b > 1)
 	{
-		printf("gate2\n");
+		//printf("gate2\n");
 		double_pi_quicksort(stackreceiver, stackgiver, nextchunk.b);//next quicksort for inferior quart		
 	}
 	push_sorted_chunk(stackreceiver, stackgiver, nextchunk.b);
-	printf("top %u int of stack %c are supposed to be sorted\n", chunksize, stackgiver->name);
-	ft_printstacks(stackgiver, stackreceiver);
+	//printf("top %u int of stack %c are supposed to be sorted\n", chunksize, stackgiver->name);
+	//ft_printstacks(stackgiver, stackreceiver);
 }
